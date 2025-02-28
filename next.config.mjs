e90bf1d4bf559e9d04b92ef6854bc45708e010a2
gmenez <73724162+germanjimenezz18@users.noreply.github.com>
2025-02-28 11:23:45 +0100

    feat: Image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,10 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    unoptimized: false, // Cambiar a false para habilitar la optimización
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ['image/webp'],
   },
   experimental: {
     webpackBuildWorker: true,
